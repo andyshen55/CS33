84889d914a07ba784d1fa84fa19ea42170f866f5
Andy Shen <andyshen55@ucla.edu>
2019-09-03 18:43:01 +0630

    added bomb lab

---
+++ Bomb Lab/bomb415/bomb.c
@@ -0,0 +1,104 @@
+/***************************************************************************
+ * The PERPETRATOR hereby grants you (the VICTIM) explicit permission to use
+ * this bomb (the BOMB).  This is a time limited license, which expires on the
+ * death of the VICTIM.  The PERPETRATOR takes no responsibility for damage,
+ * frustration, insanity, bug-eyes, carpal-tunnel syndrome, loss of sleep, or
+ * other harm to the VICTIM.  Unless the PERPETRATOR wants to take credit, that
+ * is.  The VICTIM may not distribute this bomb source code to any enemies of
+ * the PERPETRATOR.  No VICTIM may debug, reverse-engineer, run "strings" on,
+ * decompile, decrypt, or use any other technique to gain knowledge of and
+ * defuse the BOMB.  BOMB proof clothing may not be worn when handling this
+ * program.  The PERPETRATOR will not apologize for the PERPETRATOR's poor
+ * sense of humor.  This license is null and void where the BOMB is prohibited
+ * by law.
+ ***************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "support.h"
+#include "phases.h"
+
+/* 
+ * Note to self: Remember to erase this file so my victims will have no
+ * idea what is going on, and so they will all blow up in a
+ * spectaculary fiendish explosion.  ***giggles*** 
+ */
+
+FILE *infile;
+
+int main(int argc, char *argv[])
+{
+    char *input;
+
+    /* Note to self: remember to port this bomb to Windows and put a 
+     * fantastic GUI on it. */
+
+    /* When run with no arguments, the bomb reads its input lines 
+     * from standard input. */
+    if (argc == 1) {  
+	infile = stdin;
+    } 
+
+    /* When run with one argument <file>, the bomb reads from <file> 
+     * until EOF, and then switches to standard input. Thus, as you 
+     * defuse each phase, you can add its defusing string to <file> and
+     * avoid having to retype it. */
+    else if (argc == 2) {
+	if (!(infile = fopen(argv[1], "r"))) {
+	    printf("%s: Error: Couldn't open %s\n", argv[0], argv[1]);
+	    exit(8);
+	}
+    }
+
+    /* You can't call the bomb with more than 1 command line argument. */
+    else {
+	printf("Usage: %s [<input_file>]\n", argv[0]);
+	exit(8);
+    }
+
+    /* Do all sorts of secret stuff that makes the bomb harder to defuse. */
+    initialize_bomb();
+
+    welcome_message(argc);
+
+    input = read_line();             /* Get input                   */
+    phase_1(input);                  /* Run the phase               */
+    phase_defused();                 /* Drat!  They figured it out!
+				      * Let me know how they did it. */
+    printf("Phase 1 defused. And they said you'd never amount to anything. \n");
+
+    input = read_line();
+    phase_2(input);
+    phase_defused();
+    printf("That's number 2.  Why so serious?\n");
+
+    /* I guess this is too easy so far.  Some more complex code will
+     * confuse people. */
+    input = read_line();
+    phase_3(input);
+    phase_defused();
+    printf("Now that was beautiful! Halfway there!\n");
+
+    /* Oh yeah?  Well, how good is your math?  Try on this saucy problem! */
+    input = read_line();
+    phase_4(input);
+    phase_defused();
+    printf("Four stages complete? And they said I was the mad one!\n");
+    
+    /* Round and 'round in memory we go, where we stop, the bomb blows! */
+    input = read_line();
+    phase_5(input);
+    phase_defused();
+    printf("Alright good work, you'll like this last one!\n");
+
+    /* This phase will never be used, since no one will get past the
+     * earlier ones.  But just in case, make this one extra hard. */
+    input = read_line();
+    phase_6(input);
+    phase_defused();
+
+    /* Wow, they got it!  But isn't something... missing?  Perhaps
+     * something they overlooked?  Mua ha ha ha ha! */
+    
+    return 0;
+}
